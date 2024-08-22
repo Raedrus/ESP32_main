a5a42e74fafc6ede847c77ceed44b3dac230c5d4
Rae <dingyanyan483@gmail.com>
2024-08-22 14:12:58 +0800

    update

--- src/tmc2209_test.cpp
+++ src/tmc2209_test.cpp
@@ -4,16 +4,20 @@
 #include <esp_system.h>
 
 // TMC2209 stepper driver initiation
-#define DIR_PIN 23          // Direction
-#define STEP_PIN 22         // Step
-#define STALL_PIN 21        // Connected to DIAG pin on the TMC2209
+#define DIR_PIN 2          // Direction
+#define STEP_PIN 4        // Step
+#define STALL_PIN 18        // Connected to DIAG pin on the TMC2209
+#define EN_PIN 5
 #define driver_ADDRESS 0b00 // Pins MS1 and MS2 connected to GND.
-#define STALL_VALUE 80 // Stallguard values for each driver(0-255), higher number -> higher sensitivity.
+#define STALL_VALUE 58
+// Stallguard values for each driver(0-255), higher number -> higher sensitivity.
 #define RA_SENSE 0.11f      // Sense resistor value, match to your driverA
 TMC2209Stepper driver(&Serial2, RA_SENSE, driver_ADDRESS);
 AccelStepper stepper = AccelStepper(stepper.DRIVER, STEP_PIN, DIR_PIN);
-bool startup = true; // set false after homing
+
 bool stalled = false;
+
+
 
 void TMC2209settings();
 
@@ -40,14 +44,16 @@ void resetDriver()
 void TMC2209settings()
 {
   driver.begin();          // Initiate pins and registeries
-  driver.rms_current(350); // Set stepperA current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5);
+  driver.rms_current(1800); // Set stepperA current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5);
   driver.pwm_autograd(1);  // Enable automatic gradient adaptation
-  driver.pwm_autoscale(1);
-  driver.microsteps(16);
+  // driver.pwm_autoscale(1);
+  driver.microsteps(4);
   driver.TCOOLTHRS(0xFFFFF); // 20bit max
   driver.SGTHRS(STALL_VALUE);
+  
+  
 
-  stepper.setMaxSpeed(5000);
+  stepper.setMaxSpeed(10000);
   stepper.setAcceleration(2000); // 2000mm/s^2
   stepper.setPinsInverted(false, false, true);
   stepper.enableOutputs();
@@ -56,8 +62,10 @@ void TMC2209settings()
 // Function to open the gripper
 void gripperOpen()
 {
+  digitalWrite(EN_PIN,LOW);
   resetDriver();
-  stepper.setSpeed(-5000);
+  stepper.enableOutputs();
+  stepper.setSpeed(-500);
   while (!stalled)
   {
     stepper.runSpeed();
@@ -69,12 +77,23 @@ void gripperOpen()
 // Function to close the gripper
 void gripperClose()
 {
+  digitalWrite(EN_PIN,LOW);
   resetDriver();
-  stepper.setSpeed(5000);
+  stepper.enableOutputs();
+  stepper.setSpeed(500);
   while (!stalled)
   {
     stepper.runSpeed();
   }
+  
+  int i = 0;
+  while (i<15)
+  {
+  stepper.runSpeed();
+  delay(5);
+  i++;
+  }
+  
   stepper.stop();
   stepper.setCurrentPosition(0); // Set current position as home
 }
@@ -89,6 +108,12 @@ void setup()
   // Initiate serial comms with TMC2209 stepper driver
   Serial2.begin(115200);
 
+  driver.pdn_disable(1);
+  driver.ihold(5);
+
+  pinMode(DIR_PIN,OUTPUT);
+  pinMode(STEP_PIN,OUTPUT);
+  pinMode(EN_PIN,OUTPUT);
   // Enable interrupt for motor stall
   attachInterrupt(digitalPinToInterrupt(STALL_PIN), stallInterrupt, RISING);
 
@@ -101,17 +126,25 @@ void setup()
 }
 
 void loop() {
+  
+
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
     input.trim(); // Remove any leading/trailing whitespace
     Serial.print("Received command: ");
     Serial.println(input);
     if (input == "open") {
+      Serial.println("Value of pdn_uart");
+      Serial.println(driver.pdn_uart());
       Serial.println("Opening gripper...");
+      Serial.println("Value of SG_RESULT");
+      Serial.println(driver.SG_RESULT());
       gripperOpen();
       Serial.println("Gripper opened.");
     } else if (input == "close") {
       Serial.println("Closing gripper...");
+      Serial.println("Value of SG_RESULT");
+      Serial.println(driver.SG_RESULT());
       gripperClose();
       Serial.println("Gripper closed.");
     } else {
